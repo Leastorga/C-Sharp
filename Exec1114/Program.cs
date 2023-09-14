81acc454634327e53e52be423966a039bc37eaef
Leastorga <leticia.trancozo@gmail.com>
2023-09-14 09:27:54 -0300

    Exec 1114

---
+++ Exec1114/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Name{
+    class Program{
+
+        public static void Main(string[] args){
+            int senha, senhaCorreta;
+        
+            senha = int.Parse(Console.ReadLine());
+            senhaCorreta = 2002;
+
+            while (senha != senhaCorreta){
+                if(senha != senhaCorreta){
+                Console.WriteLine("Senha Invalida");
+                senha = int.Parse(Console.ReadLine());                    
+                }    
+            }
+                Console.WriteLine("Acesso Permitido");
+        }
+    }
+}
