aedd2c8214663fbf0fd7bc8eec47cb13dbceb7a4
Leastorga <leticia.trancozo@gmail.com>
2023-09-14 11:16:11 -0300

    Exec 1154

---
+++ Exec1154/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Globalization;
+
+namespace Name
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            double idade, media, soma;
+            int contador;
+
+            soma = 0.0;
+            contador = 0;
+
+            idade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            while (idade > 0.0)
+            {
+                soma = soma + idade;
+                contador += 1;
+                idade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            media = soma / contador;
+            Console.WriteLine(media.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
