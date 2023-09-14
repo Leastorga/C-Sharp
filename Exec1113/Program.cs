8feb6276ddf4d3cbecf6747fa89232f260455e08
Leastorga <leticia.trancozo@gmail.com>
2023-09-14 08:22:25 -0300

    Exec 1113

---
+++ Exec1113/Program.cs
@@ -0,0 +1,29 @@
+﻿
+namespace Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            string[] vet = Console.ReadLine().Split(' ');
+            x = int.Parse(vet[0]);
+            y = int.Parse(vet[1]);
+
+            while (x != y)
+            {
+                if (x > y)
+                {
+                    Console.WriteLine("Decrescente");
+                }
+                else
+                {
+                    Console.WriteLine("Crescente");
+                }
+                vet = Console.ReadLine().Split(' ');
+                x = int.Parse(vet[0]);
+                y = int.Parse(vet[1]);
+            }
+        }
+    }
+}
