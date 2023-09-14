9752052bc3bc6934acc2d3189fd1561c959f142e
Leastorga <leticia.trancozo@gmail.com>
2023-09-14 13:45:45 -0300

    Exec 1131

--- Exec1131/Program.cs
+++ Exec1131/Program.cs
@@ -1,1 +1,55 @@
-﻿
+﻿namespace Name
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int gremio = 0;
+            int inter = 0;
+            int empates = 0;
+            int resposta = 1;
+
+
+            while (resposta == 1)
+            {
+                string[] vet = Console.ReadLine().Split(' ');
+                int pt1 = int.Parse(vet[0]);
+                int pt2 = int.Parse(vet[1]);
+
+                if (pt1 == pt2)
+                {
+                    empates += 1;
+                }
+                else if (pt1 > pt2)
+                {
+                    inter += 1;
+                }
+                else
+                {
+                    gremio += 1;
+                }
+                Console.WriteLine("Novo grenal (1-sim 2-nao)");
+                resposta = int.Parse(Console.ReadLine());
+            }
+
+            int jogos = gremio + inter + empates;
+
+            Console.WriteLine(jogos + " grenais");
+            Console.WriteLine("Inter:" + inter);
+            Console.WriteLine("Gremio:" + gremio);
+            Console.WriteLine("Empates:" + empates);
+
+            if (inter > gremio)
+            {
+                Console.WriteLine("Inter venceu mais");
+            }
+            else if (gremio > inter)
+            {
+                Console.WriteLine("Gremio venceu mais");
+            }else
+            {
+                Console.WriteLine("Nao houve vencedor");
+            }
+        }
+    }
+}
