51d3341a83eceaa5963185d2724fdb53626325e0
Leastorga <leticia.trancozo@gmail.com>
2023-09-14 09:12:07 -0300

    Exec resolvidos

---
+++ ExercResolvido01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+
+namespace Name{
+    class Program{
+        static void Main(string[] args){
+            double nota1, nota2, notaFinal;
+
+            Console.WriteLine("Digite a nota do primeiro semestre:");
+            nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Digite a nota do segundo semestre:");
+            nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            notaFinal = nota1 + nota2;
+            Console.WriteLine("NOTA FINAL = " + notaFinal.ToString("F1", CultureInfo.InvariantCulture));
+
+            if (notaFinal < 60.0){
+                Console.WriteLine("REPROVADO!");
+            }
+        }
+        
+    }
+    
+}
---
+++ ExercResolvido03/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Globalization;
+
+namespace Name{
+    class Program{
+        public static void Main(string[] args){
+
+            double idade, soma, media;
+            int contagem;
+
+            idade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            soma = 0.0;
+            contagem = 0;
+
+            while (idade >= 0){
+                soma = soma + idade;
+                contagem = contagem + 1;
+                idade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+            if(contagem == 0){
+                Console.WriteLine("impossivel calcular");
+            }else
+            {
+            media = soma / contagem;
+            Console.WriteLine(media.ToString("F2", CultureInfo.InvariantCulture));                
+            }
+        }
+    }
+}
