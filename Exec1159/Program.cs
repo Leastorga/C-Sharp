edcc130be097d1572b2d17f355e41d95e29697ae
Leastorga <leticia.trancozo@gmail.com>
2023-09-14 11:38:15 -0300

    Exec 1159

---
+++ Exec1159/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Name
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int x, par;
+
+            x = int.Parse(Console.ReadLine());
+
+            while (x != 0)
+            {
+                if (x % 2 == 0)
+                {
+                    par = x + x + 2 + x + 4 + x + 6 + x + 8;
+                }
+                else
+                {
+                    par = x + 1 + x + 3 + x + 5 + x + 7 + x + 9;
+                }
+                Console.WriteLine(par);
+                x = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
