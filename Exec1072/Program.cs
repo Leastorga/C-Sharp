15acf906612d35be10b252886043617145cea54f
Leastorga <leticia.trancozo@gmail.com>
2023-09-15 10:00:37 -0300

    Exec1072

---
+++ Exec1072/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Name
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+  
+            int dentro = 0;        
+            int fora = 0;        
+
+            for (int i = 1; i < n; i++)
+            {
+            int x = int.Parse(Console.ReadLine());                  
+                if (x >= 10 && x <= 20)
+                {
+                    dentro = dentro + 1;
+                }else{
+                    fora = fora + 1;
+                }
+            }
+
+            Console.WriteLine(dentro + " in");
+            Console.WriteLine(fora + " out");
+        }
+    }
+}
