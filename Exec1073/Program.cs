1a21945514a5c6aea72006d17981485bd811ceb1
Leastorga <leticia.trancozo@gmail.com>
2023-09-15 10:40:32 -0300

    Exec1073

---
+++ Exec1073/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Name
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {   double resultado;
+            int n = int.Parse(Console.ReadLine());
+            
+            for (int i = 1; i <= n ; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    resultado = Math.Pow(i,2);
+                    Console.WriteLine(i + "^2 = " + resultado);
+                }
+            }
+        }
+    }
+}
