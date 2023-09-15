3d09a5c98f30146d9be025f55a2a57c6fb61b03f
Leastorga <leticia.trancozo@gmail.com>
2023-09-15 08:39:00 -0300

    Exec 1078

---
+++ Exec1078/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Name
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int N, multiplicacao, numero;
+
+            N = int.Parse(Console.ReadLine());
+            numero = 1;
+
+            for (int i = 0; i <= 9; i++)
+            {
+                multiplicacao = numero * N;
+                Console.WriteLine(numero + " x " + N + " = " + multiplicacao);
+                numero += 1;                
+            }
+        }
+    }
+}
