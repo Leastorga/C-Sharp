c8a361f925fbec1f08f4e19221ffe7a5b8c7eb9d
Leastorga <leticia.trancozo@gmail.com>
2023-09-15 11:00:27 -0300

    Exec1075

---
+++ Exec1075/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Name
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            
+            for (int i = 1; i <= 10000 ; i++)
+            {
+                if ( i % n == 2)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
