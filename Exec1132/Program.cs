43b5bcb1a88ebaac778baf241974c84904a6095f
Leastorga <leticia.trancozo@gmail.com>
2023-09-18 08:58:42 -0300

    Exec1132

---
+++ Exec1132/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Name
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            int maior = y;
+            int menor = x;
+
+            if (x > y)
+            {
+                maior = x;
+                menor = y;
+            }
+
+            int soma = 0;
+            for (int i = menor; i <= maior; i++)
+            {
+                if (i % 13 != 0)
+                {
+                    soma =  soma + i; 
+                }
+            }
+            Console.WriteLine(soma);
+        }
+    }
+}
