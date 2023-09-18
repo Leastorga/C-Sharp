79f3b21027a0bd71fd0e79a1f369f7a5c2d2938b
Leastorga <leticia.trancozo@gmail.com>
2023-09-18 11:09:41 -0300

    Exec1174 E Exec1144

---
+++ Exec1074/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Name
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            
+            for (int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+
+                if (x % 2 == 0 && x > 0)
+                {
+                    Console.WriteLine("EVEN POSITIVE");
+                }else if(x % 2 == 0 && x < 0)
+                {
+                    Console.WriteLine("EVEN NEGATIVE");
+                    
+                }else if(x % 2 != 0 && x < 0)
+                {
+                    Console.WriteLine("ODD NEGATIVE");
+                }else if(x % 2 != 0 && x > 0)
+                {
+                    Console.WriteLine("ODD POSITIVE");
+                }
+                else
+                {
+                    Console.WriteLine("NULL");
+                }
+            }
+        }
+    }
+}
---
+++ Exec1144/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Name
+{
+    class MyClass
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n ; i++)
+            {
+                double x = Math.Pow(i,2);
+                double y = Math.Pow(i,3);
+                Console.WriteLine(i + " " + x + " " + y );
+
+                x ++;
+                y ++;
+                Console.WriteLine(i + " " + x + " "+ y);
+
+            }
+        }
+    }
+}
