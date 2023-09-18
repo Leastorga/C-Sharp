838e3328b22dddca1d317c680c0792fa40e66bd7
Leastorga <leticia.trancozo@gmail.com>
2023-09-18 10:17:53 -0300

    Exec1142

---
+++ Exec1142/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Name
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int x = 1;
+
+            for (int i = 1; i <= n; i ++)
+
+            {
+                int y = x + 1;
+                int z = x + 2;
+                Console.WriteLine(x + " "+ y + " "+ z + " PUM"); 
+
+                x = z + 2;
+            }
+        }
+    }
+}
