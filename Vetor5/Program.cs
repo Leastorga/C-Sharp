6bdcef81e2181613d63a8a8ec1d33a5dd3b0e92d
Leastorga <leticia.trancozo@gmail.com>
2023-09-19 15:01:48 -0300

    Exercício resolvido vetor 5

---
+++ Vetor5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+namespace Name
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            double[] vet = new double[n];
+            
+            double soma = 0;
+            int contador = 0;
+
+            string[] elemento = Console.ReadLine().Split(' ');
+            for (int i = 0; i < n; i++)
+            {
+                vet[i] = int.Parse(elemento[i]);
+                if (vet[i] % 2 == 0)
+                {
+                    soma = soma + vet[i];
+                    contador ++;
+                }
+            }
+            double media = soma / contador;
+            Console.WriteLine(media.ToString("F1", CultureInfo.InvariantCulture));            
+        }
+    }
+}
