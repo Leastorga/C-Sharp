618917b687d625109ef3c6b4a8a65a4b868590c4
Leastorga <leticia.trancozo@gmail.com>
2023-09-19 15:49:21 -0300

    Exercício resolvido vetor 7

---
+++ Vetor7/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Globalization;
+
+namespace Name
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string[] nome = new string[n];
+            double[] nota1 = new double[n];
+            double[] nota2 = new double[n];
+
+            
+            for (int i = 0; i < n; i++)
+            {
+                string[] elementos = Console.ReadLine().Split(' ');
+                nome[i] = elementos[0];
+                nota1[i] = double.Parse(elementos[1], CultureInfo.InvariantCulture);
+                nota2[i] = double.Parse(elementos[2], CultureInfo.InvariantCulture);
+            }
+
+            Console.WriteLine("Alunos aprovados: ");
+            for (int i = 0; i < n; i++)
+            {
+                double media = (nota1[i] + nota2[i]) / 2.0;
+                if (media >= 6.0)
+                {
+                    Console.WriteLine(nome[i]);
+                }
+            }
+        }
+    }
+}
