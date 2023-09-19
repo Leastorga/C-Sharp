a28814ed414da1ccf073dd28bbbc72a41a77c049
Leastorga <leticia.trancozo@gmail.com>
2023-09-19 14:30:56 -0300

    exercícios resolvidos veto 3

---
+++ Vetor3/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Name
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] a = new int[n];
+            int[] b = new int[n];
+            
+
+            string[] vet = Console.ReadLine().Split(' ');
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = int.Parse(vet[i]);
+            }
+
+            vet = Console.ReadLine().Split(' ');
+            for (int i = 0; i < n; i++)
+            {
+                b[i] = int.Parse(vet[i]);
+            }
+
+            int[] c = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                c[i] = a[i] + b[i]; 
+                Console.Write(c[i] + " ");
+            }
+
+        }
+    }
+}
