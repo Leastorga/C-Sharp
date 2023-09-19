e584bfdbeb75f289e1b566fec64cd1260e9d94b3
Leastorga <leticia.trancozo@gmail.com>
2023-09-19 10:27:48 -0300

    vetores exercício resolvido 1

---
+++ VetorResolvido1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Name
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] vet;
+            int n = int.Parse(Console.ReadLine());
+            vet = new int[n];
+
+          
+            string[] s = Console.ReadLine().Split(' ');
+            for (int i = 0; i < n; i++)
+            {
+                vet[i] = int.Parse(s[i]);
+            }                        
+
+            for (int i = 0; i < n; i++)
+            {
+                if (vet[i] < 0)
+                {
+                    Console.WriteLine(vet[i]);
+                }
+            }
+        }
+    }
+}
