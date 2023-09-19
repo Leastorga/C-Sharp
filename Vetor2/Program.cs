b7ba348a2f6e9ee4997a47a6c5029c5c25060125
Leastorga <leticia.trancozo@gmail.com>
2023-09-19 14:08:17 -0300

    Exercicio resolvido vetor 2

---
+++ Vetor2/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Name
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int contador = 0;
+            int n = int.Parse(Console.ReadLine());
+            int[] num = new int[n];
+
+            string[] vet = Console.ReadLine().Split(' ');
+            for (int i = 0; i < n; i++)
+            {
+
+                num[i] = int.Parse(vet[i]);
+
+                if (num[i] % 2 == 0)
+                {
+                    Console.Write(num[i] + " ");
+                    contador++;
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine(contador);
+
+        }
+    }
+}
