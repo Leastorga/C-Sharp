21777f2beff414b6a0ee194e264f35da260f7eed
Leastorga <leticia.trancozo@gmail.com>
2023-09-19 15:21:21 -0300

    Exercício resolvido vetor 9

---
+++ Vetor6/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Name
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string[] nome = new string[n];
+            int[] idade = new int[n];
+
+            string[] elemento = Console.ReadLine().Split(' ');
+            for (int i = 0; i < n; i++)
+            {
+                nome[i] = elemento[0];
+                idade[i] = int.Parse(elemento[1]);
+            }
+
+            int maior = idade[0];
+            int contador = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (idade[i] > maior )
+                {
+                    maior = idade[i];
+                    contador = i;
+                }
+            }
+            Console.WriteLine("Pessoas mais velha: " + nome[contador]);
+
+        }
+    }
+}
