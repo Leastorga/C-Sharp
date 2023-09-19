bd72763aea4a7767bc97f90a5152b29fbba80aed
Leastorga <leticia.trancozo@gmail.com>
2023-09-19 12:51:22 -0300

    Exercício resolvido vetor 1

---
+++ Vetor1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+
+namespace Name
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            double maior = 0.0;
+            int contador = 0;
+            int n = int.Parse(Console.ReadLine());
+            double[] numero = new double[n];
+
+            string[] vet = Console.ReadLine().Split(' ');
+            for (int i = 0; i < n; i++)
+            {
+                numero[i] = double.Parse(vet[i], CultureInfo.InvariantCulture);
+
+                if (maior < numero[i])
+                {
+                    maior = numero[i];
+                    contador++;
+                }
+            }
+            Console.WriteLine(maior.ToString("F1", CultureInfo.InvariantCulture));
+            Console.WriteLine(contador);
+        }
+    }
+}
