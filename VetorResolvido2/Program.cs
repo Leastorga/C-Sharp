d2dfd8c8618ee6c3d1ea1c8014bc3a721cfed880
Leastorga <leticia.trancozo@gmail.com>
2023-09-19 10:49:07 -0300

    vetores exercício resolvido 2

---
+++ VetorResolvido2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+
+namespace Name
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            double[] vet;
+            double soma = 0.0;
+            double media = 0.0;
+
+
+            int n = int.Parse(Console.ReadLine());
+            vet = new double[n];
+
+            string[] leitor = Console.ReadLine().Split(' ');
+
+            for (int i = 0; i < n; i++)
+            {
+                vet[i] = double.Parse(leitor[i], CultureInfo.InvariantCulture);
+                
+            }
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(vet[i].ToString("F1", CultureInfo.InvariantCulture) + " ");
+                soma = vet[i] + soma;
+            }
+            Console.WriteLine();
+
+            media = soma / n;
+            Console.WriteLine(soma.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(media.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
