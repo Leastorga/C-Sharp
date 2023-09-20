5da0b5b24c681bcc967e84d5d48d4602e1a31802
Leastorga <leticia.trancozo@gmail.com>
2023-09-20 11:46:07 -0300

    Exercicio proposto martriz 2

---
+++ Matriz2/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Name
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] a = new int[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                string[] elemento = Console.ReadLine().Split(' ');
+                for (int j = 0; j < n; j++)
+                {
+                    a[i,j] = int.Parse(elemento[j]);
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                int soma = 0;
+                for (int j = 0; j < n; j++)
+                {
+                    soma = soma + a[i,j];
+                }
+                Console.WriteLine(soma);
+            }
+        }
+    }
+}
