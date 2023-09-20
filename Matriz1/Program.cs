688f0099abb7769cb485dee274bbbc37982c0d7f
Leastorga <leticia.trancozo@gmail.com>
2023-09-20 11:36:04 -0300

    Exercicio proposto martriz 1

--- Matriz1/Program.cs
+++ Matriz1/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Name
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] elemento = Console.ReadLine().Split(' ');
+            int m = int.Parse(elemento[0]);
+            int n = int.Parse(elemento[1]);
+            
+            int[,] matriz = new int[m,n];
+            for (int i = 0; i < m; i++)
+            {
+                string[] elemento1 = Console.ReadLine().Split(' ');
+                for (int j = 0; j < n; j++)
+                {
+                    matriz[i,j] = int.Parse(elemento1[j]);
+                }
+            }
+
+            Console.WriteLine("VALORES NEGATIVOS: ");
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (matriz[i,j] < 0)
+                    {
+                        Console.WriteLine(matriz[i,j]);
+                    }
+                }
+            }
+
+        }
+    }
+}
