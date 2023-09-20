b2bc8e053578b1c121d25fbdeb7245af36e75f2b
Leastorga <leticia.trancozo@gmail.com>
2023-09-20 12:15:13 -0300

    Exercicio proposto martriz 3

---
+++ MatrizResolvido3/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Name
+{
+    class Name{
+        public static void Main(string[] args)
+        {
+         int n = int.Parse(Console.ReadLine());
+
+         int[,] a = new int [n,n];
+         for (int i = 0; i < n; i++)
+         {
+            string[] elemento = Console.ReadLine().Split(' ');
+            for (int j = 0; j < n; j++)
+            {
+                a[i,j] = int.Parse(elemento[j]);
+            }
+         }   
+
+         for (int i = 0; i < n; i++)
+         {
+            int maior = 0;
+            for (int j = 0; j < n; j++)
+            {            
+                if (a[i,j] > maior)
+                {
+                    maior = a[i,j];
+                }                
+            }
+            Console.WriteLine(maior);
+         }
+        }
+    }
+}
