e05092bcc02216ebfffb1d838b4bf331f97165a8
Leastorga <leticia.trancozo@gmail.com>
2023-09-20 13:21:11 -0300

    Exercicio proposto martriz 4

---
+++ MatrizResolvido4/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Name
+{
+    class MyClass
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] matriz = new int[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] elemento = Console.ReadLine().Split(' '); ;
+                for (int j = 0; j < n; j++)
+                {
+                    matriz[i, j] = int.Parse(elemento[j]);
+                }
+            }
+
+            int soma = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                { 
+                    soma = soma + matriz[i, j];
+                }                
+            }
+            Console.WriteLine(soma);
+        }
+    }
+}
